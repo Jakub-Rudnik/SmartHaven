12ea7102c2493e7114c1355c8afd02189b1d9e73
JakubKlipkon <jakub.rudnik@siecbudowlanaexport.pl>
2024-11-08 12:53:09 +0100

    refactor: changed layout and removed old demo

--- index.php
+++ index.php
@@ -1,43 +1,16 @@
-<?php
-require_once './Lib/Database.php';
-require_once './Services/DeviceService.php';
-require_once './Services/DeviceTypeService.php';
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smarthaven</title>
+    <link rel="stylesheet" href="/styles/main.min.css">
+</head>
+<body class="p-3 vw-100 vh-100">
+   
+    <?php 
+        require_once './UI/components/navbar.php';
+    ?>
 
-$db = new DatabaseConnection();
-
-$devicesService = new DeviceService($db);
-$devicesTypeService = new DeviceTypeService($db);
-
-$devices = $devicesService->getDevices();
-
-echo '<h2>Devices</h2>';
-foreach ($devices as $device) {
-    echo $device->getName() . ' ' . $device->getType()->getName() . ' ' . $device->getState() . '<br>';
-}
-
-echo '<h2>By Id</h2>';
-$device = $devicesService->getDeviceById(1);
-echo $device->getName() . ' ' . $device->getType()->getName() . ' ' . $device->getState() . '<br>';
-
-echo '<h2>By Name</h2>';
-$device = $devicesService->getDeviceByName('Tv');
-echo $device->getName() . ' ' . $device->getType()->getName() . ' ' . $device->getState() . '<br>';
-
-echo '<h2>By Type</h2>';
-$deviceType = $devicesTypeService->getDeviceTypeByName('Light');
-$devices = $devicesService->getDeviceByType($deviceType);
-foreach ($devices as $device) {
-    echo $device->getName() . ' ' . $device->getType()->getName() . ' ' . $device->getState() . '<br>';
-}
-
-echo '<h2>By State</h2>';
-$devices = $devicesService->getDeviceByState(true);
-foreach ($devices as $device) {
-    echo $device->getName() . ' ' . $device->getType()->getName() . ' ' . $device->getState() . '<br>';
-}
-
-echo '<h2>Device Types</h2>';
-$devicesTypes = $devicesTypeService->getDeviceTypes();
-foreach ($devicesTypes as $deviceType) {
-    echo $deviceType->getId() . ' ' .  $deviceType->getName() . '<br>';
-}
+</body>
+</html>
