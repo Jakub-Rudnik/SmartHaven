e4ab10084b264752ee3f8987259d8c30a48f7920
jakub-rudnik <jakub.rudnik@interia.pl>
2024-11-08 18:28:42 +0100

    feat: new responsive layout

--- index.php
+++ index.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Smarthaven</title>
-    <link rel="stylesheet" href="/styles/main.min.css">
+    <link rel="stylesheet" href="/styles/main.css">
 </head>
-<body class="p-3 vw-100 vh-100">
+<body class="d-flex flex-md-row p-3 gap-3 w-100 vh-100">
    
-    <?php 
+    <?php
+        require_once './UI/components/header.php';
         require_once './UI/components/navbar.php';
     ?>
+
+
+    <main class="card bg-dark-subtle flex-grow-1 p-4" style="min-height: 100%">
+        <?php
+            createHeader('Cześć Dawid! 😎');
+        ?>
+    </main>
 
 </body>
 </html>
