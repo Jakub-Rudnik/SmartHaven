2f2ef19376c178f3410c598d52d18afa17762d4a
jakub-rudnik <jakub.rudnik@interia.pl>
2024-11-15 03:01:48 +0100

    feat: add getter for id

--- Entity/Device.php
+++ Entity/Device.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace Entity;
 
-class Device {
+class Device
+{
     private int $id;
     private string $name;
     private DeviceType $type;
     private ?string $room;
     private bool $state;
 
-    public function __construct(int $id, string $name, DeviceType $type, ?string $room, bool $state) {
+    public function __construct(int $id, string $name, DeviceType $type, ?string $room, bool $state)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->type = $type;
@@ -16,19 +19,28 @@ class Device {
         $this->state = $state;
     }
 
-    public function getName(): string {
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getRoom(): ?string {
+    public function getRoom(): ?string
+    {
         return $this->room;
     }
 
-    public function getState(): bool {
+    public function getState(): bool
+    {
         return $this->state;
     }
 
-    public function getType(): DeviceType {
+    public function getType(): DeviceType
+    {
         return $this->type;
     }
 }
