4c15ecae2dbb47ab574e9d215240baa6cf8d35cc
jakub-rudnik <jakub.rudnik@interia.pl>
2024-11-22 00:17:18 +0100

    feat: register api route to autoload

--- autoload.php
+++ autoload.php
@@ -3,6 +3,7 @@
 require_once 'Autoloader.php';
 
 // Register your source directory
+Autoloader::register('Api', __DIR__.'/Api');
 Autoloader::register('Services', __DIR__.'/Services');
 Autoloader::register('Lib', __DIR__.'/Lib');
 Autoloader::register('Interfaces', __DIR__.'/Interfaces');
